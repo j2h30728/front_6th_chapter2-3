da1347c42f4907387d8fb011a4b1f197e45ea024
jihyeon <rachel2148072@gmail.com>
2025-08-14 01:17:19 +0900

    feat: feature/comment-like 생성

--- src/feature/comment-like/api/index.ts
+++ src/feature/comment-like/api/index.ts
@@ -1,9 +1,37 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+
 import type { Comment } from "@/entities/comment"
 
 import { commentApi } from "@/entities/comment"
 
-export const commentLikeApi = {
-  async incrementLike(comment: Pick<Comment, "id" | "likes">) {
-    return commentApi.updateLike(comment)
-  },
+export const useCommentLikeMutation = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: (comment: Pick<Comment, "id" | "likes" | "postId">) => commentApi.updateLike(comment),
+
+    onError: (err, variables, context) => {
+      const typedContext = context as { previousComments?: Comment[] }
+      if (typedContext.previousComments) {
+        queryClient.setQueryData(["comments", variables.postId], typedContext.previousComments)
+      }
+    },
+
+    onMutate: async ({ id, likes, postId }) => {
+      await queryClient.cancelQueries({ queryKey: ["comments", postId] })
+
+      const previousComments = queryClient.getQueryData(["comments", postId])
+
+      queryClient.setQueryData(["coments", postId], (old: Comment[]) =>
+        old?.map((comment) => (comment.id === id ? { ...comment, likes: likes + 1 } : comment)),
+      )
+      return previousComments
+    },
+
+    onSettled: (data, error, variables) => {
+      queryClient.invalidateQueries({
+        queryKey: ["comments", variables.postId],
+      })
+    },
+  })
 }
